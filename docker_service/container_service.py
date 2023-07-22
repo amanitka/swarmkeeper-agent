c399b7edf1b3b3dfdb6a04ff03066871898eb388
amanitka <ballnazzar@gmail.com>
2023-07-22 22:03:43 +0200

    Add checking of standalone containers

--- docker_service/container_service.py
+++ docker_service/container_service.py
@@ -22,20 +22,45 @@ class ContainerService:
         logging.info("Cleanup unused images")
         self.__docker_api.remove_unused_image()
 
-    def cleanup(self):
-        if self.__container_cleanup:
-            self.__remove_stopped_container()
-        if self.__image_cleanup:
-            self.__remove_unused_image()
-
-    def report_container_status(self):
-        logging.info("Report docker_service status to swarmkeeper")
-        container_list: list[dict] = self.__docker_api.get_running_container_list()
+    def __report_service_container_status(self, service_container_list: list[dict]):
+        logging.info("Report service container status to swarmkeeper")
         try:
-            response: Response = requests.post(f"{self.__swarmkeeper_url}/api/status/container", json=container_list)
+            response: Response = requests.post(f"{self.__swarmkeeper_url}/api/status/container", json=service_container_list)
             if response.status_code == 200:
                 logging.info(f"Container status was sent to swarmkeeper. Response: {response.json()}")
             else:
                 logging.error(f"Unable to send docker_service status to swarmkeeper. Http status: {response.status_code}, text: {response.text}")
         except Exception as e:
             logging.error(f"Unable to send docker_service status to swarmkeeper. Error: {e}")
+
+    @staticmethod
+    def __can_process_container(container: dict) -> bool:
+        if container["process_flag"] == "N":
+            logging.info(f"Skip processing of container {container['name']}, because it was disabled label")
+            return False
+        else:
+            return True
+
+    def __process_container(self, container: dict):
+        if self.__can_process_container(container):
+            image_digest_repository: str = self.__docker_api.get_image_digest_registry(container["image_tag"])
+            if image_digest_repository in container["image_digest_list"]:
+                logging.info(f"Container {container['name']} is using up to date image")
+            else:
+                logging.info(f"Container {container['name']} is using outdated image and should be updated!")
+
+    def __process_container_list(self, non_service_container_list: list[dict]):
+        logging.info("Process containers which doesn't belong to any service")
+        for container in non_service_container_list:
+            self.__process_container(container)
+
+    def cleanup(self):
+        if self.__container_cleanup:
+            self.__remove_stopped_container()
+        if self.__image_cleanup:
+            self.__remove_unused_image()
+
+    def process_running_container(self):
+        service_container_list, container_list = self.__docker_api.get_running_container_list()
+        self.__report_service_container_status(service_container_list)
+        self.__process_container_list(container_list)
--- docker_service/docker_api.py
+++ docker_service/docker_api.py
@@ -1,7 +1,8 @@
-
 import logging
-from config.config import config
 import docker
+from docker.models.containers import Container
+
+from config.config import config
 
 
 class DockerApi:
@@ -10,6 +11,16 @@ class DockerApi:
         docker_base_url: str = config.get("DEFAULT", "docker_base_url")
         logging.info(f"Initialize docker_service client with base url: {docker_base_url}")
         self.__client = docker.DockerClient(base_url=docker_base_url)
+
+    @staticmethod
+    def __get_swarmkeeper_flag_from_label(container: Container) -> str:
+        if "swarmkeeper.enable" in container.labels:
+            if container.labels["swarmkeeper.enable"] == "true":
+                return "Y"
+            else:
+                return "N"
+        else:
+            return "X"
 
     @staticmethod
     def __get_image_digest_for_container(image_id: str, image_dict: dict) -> list[str]:
@@ -29,6 +40,10 @@ class DockerApi:
         if result["ImagesDeleted"] is not None:
             logging.info(f"Removed images: {result['ImagesDeleted']}, reclaimed space: {result['SpaceReclaimed']}")
 
+    def get_image_digest_registry(self, image_tag: str) -> str:
+        image_registry_data = self.__client.images.get_registry_data(image_tag)
+        return image_registry_data.id
+
     def get_image_dict(self) -> dict:
         image_dict: dict = {}
         image_list = self.__client.images.list()
@@ -37,15 +52,23 @@ class DockerApi:
                                     "image_digest_list": image.attrs.get("RepoDigests")}
         return image_dict
 
-    def get_running_container_list(self) -> list[dict]:
-        logging.info("Retrieve running docker service list")
+    def get_running_container_list(self) -> tuple[list[dict], list[dict]]:
+        logging.info("Retrieve running docker container list")
         image_dict: dict = self.get_image_dict()
+        service_container_list: list[dict] = []
         container_list: list[dict] = []
         for container in self.__client.containers.list():
-            container_list.append({"id": container.id,
-                                   "name": container.name,
-                                   "image_digest_list": self.__get_image_digest_for_container(container.image.id, image_dict),
-                                   "stack_namespace": container.labels["com.docker.stack.namespace"] if "com.docker.stack.namespace" in container.labels else "",
-                                   "id_service": container.labels["com.docker.swarm.service.id"] if "com.docker.swarm.service.id" in container.labels else "",
-                                   "service_name": container.labels["com.docker.swarm.service.name"] if "com.docker.swarm.service.name" in container.labels else ""})
-        return container_list
+            if "com.docker.swarm.service.id" in container.labels:
+                service_container_list.append({"id": container.id,
+                                               "name": container.name,
+                                               "image_digest_list": self.__get_image_digest_for_container(container.image.id, image_dict),
+                                               "stack_namespace": container.labels["com.docker.stack.namespace"] if "com.docker.stack.namespace" in container.labels else "",
+                                               "id_service": container.labels["com.docker.swarm.service.id"] if "com.docker.swarm.service.id" in container.labels else "",
+                                               "service_name": container.labels["com.docker.swarm.service.name"] if "com.docker.swarm.service.name" in container.labels else ""})
+            else:
+                container_list.append({"id": container.id,
+                                       "name": container.name,
+                                       "image_tag": container.image.tags[0] if container.image.tags else "",
+                                       "image_digest_list": self.__get_image_digest_for_container(container.image.id, image_dict),
+                                       "process_flag": self.__get_swarmkeeper_flag_from_label(container)})
+        return service_container_list, container_list
